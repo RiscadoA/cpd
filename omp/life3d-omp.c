ee9f0ab89f9e58c1630dfd1c7eca3738529ff784
Ricardo Antunes <me@riscadoa.com>
2024-03-02 10:05:04 +0000

    feat(omp): update code from new serial

--- omp/life3d-omp.c
+++ omp/life3d-omp.c
@@ -36,7 +36,7 @@ float get_random() {
  * @param N Side length of the grid.
  */
 unsigned char *alloc_grid(int N) {
-  size_t side = (size_t)N;
+  size_t side = (size_t)N + 2;
   return (unsigned char *)malloc(side * side * side * sizeof(unsigned char));
 }
 
@@ -45,6 +45,22 @@ unsigned char *alloc_grid(int N) {
  * @param grid Grid to be freed.
  */
 void free_grid(unsigned char *grid) { free(grid); }
+
+// Map a 3D coordinate to a linear index in the grid array.
+#define linear_from_3d(S, x, y, z) ((x) * (S) * (S) + (y) * (S) + (z))
+
+// Read the value of a neighbor cell, given the linear index of the current cell and the
+// displacement in the x, y and z axes.
+#define read_neighbor(grid, S, c, dx, dy, dz) grid[(c) + linear_from_3d((S), (dx), (dy), (dz))]
+
+// We use a double !! to convert the species count to a boolean value (0 or 1).
+#define is_neighbor_alive(grid, S, c, dx, dy, dz)                                                  \
+  !!read_neighbor((grid), (S), (c), (dx), (dy), (dz))
+
+// We use this macro to compute the maximum of two numbers without branching.
+// We decided to use it after discovering during profiling that the branch
+// miss rate was very high in the original code.
+#define fast_max(x, y) ((x) - (((x) - (y)) & ((x) - (y)) >> 31))
 
 /**
  * @brief Randomize the grid with a given density.
@@ -55,42 +71,12 @@ void free_grid(unsigned char *grid) { free(grid); }
  */
 void randomize_grid(unsigned char *grid, int N, float density, int input_seed) {
   init_random(input_seed);
-  for (int x = 0; x < N; x++)
-    for (int y = 0; y < N; y++)
-      for (int z = 0; z < N; z++)
+  for (int x = 1; x <= N; x++)
+    for (int y = 1; y <= N; y++)
+      for (int z = 1; z <= N; z++)
         if (get_random() < density)
-          grid[x * N * N + y * N + z] = (unsigned char)(get_random() * N_SPECIES) + 1;
+          grid[linear_from_3d(N + 2, x, y, z)] = (unsigned char)(get_random() * N_SPECIES) + 1;
 }
-
-/**
- * @brief Accesses a cell in the grid.
- * @param grid Grid to be accessed.
- * @param N Side length of the grid.
- * @param x X coordinate of the cell.
- * @param y Y coordinate of the cell.
- * @param z Z coordinate of the cell.
- */
-#define read_grid(grid, N, x, y, z) grid[x * N * N + y * N + z]
-
-/**
- * @brief Sets a cell in the grid.
- * @param grid Grid to be accessed.
- * @param N Side length of the grid.
- * @param x X coordinate of the cell.
- * @param y Y coordinate of the cell.
- * @param z Z coordinate of the cell.
- * @param value Value to be set.
- */
-#define write_grid(grid, N, x, y, z, value) (grid[x * N * N + y * N + z] = value)
-
-#define wrap_around(N, c) ((c + N) % N)
-
-#define linear_from_3d(N, x, y, z)                                                                 \
-  (wrap_around(N, x) * N * N + wrap_around(N, y) * N + wrap_around(N, z))
-
-#define read_neighbor(grid, N, x, y, z, dx, dy, dz) grid[linear_from_3d(N, x + dx, y + dy, z + dz)]
-
-#define fast_max(x, y) (x - ((x - y) & (x - y) >> 31))
 
 int main(int argc, char **argv) {
   if (argc != 5) {
@@ -113,12 +99,15 @@ int main(int argc, char **argv) {
   unsigned char *next = alloc_grid(N);
   randomize_grid(previous, N, density, seed);
 
+  /* Start tracking time */
+  double time = -omp_get_wtime();
+
   /* Count how many of each species is alive */
   long long total_count[N_SPECIES + 1] = {0};
-  for (int x = 0; x < N; ++x) {
-    for (int y = 0; y < N; ++y) {
-      for (int z = 0; z < N; ++z) {
-        total_count[read_grid(previous, N, x, y, z)] += 1;
+  for (int x = 1; x <= N; ++x) {
+    for (int y = 1; y <= N; ++y) {
+      for (int z = 1; z <= N; ++z) {
+        total_count[previous[linear_from_3d(N + 2, x, y, z)]] += 1;
       }
     }
   }
@@ -131,105 +120,152 @@ int main(int argc, char **argv) {
     total_count[i] = 0;
   }
 
-  /* Start tracking time */
-  double time = -omp_get_wtime();
-
   /* Run the simulation */
   for (int g = 1; g <= generations; g++) {
+    /**
+     * README:
+     *
+     * To reduce the complexity of the boundary conditions (wrap around), instead of performing
+     * expensive modulus and division operations every neighbor access, we add a border of 1 cell
+     * around the grid. This border is initialized with the same values as the opposite side of the
+     * grid, so that the boundary conditions are satisfied.
+     * 
+     * The following three loops correspond to that initialization.
+     */
+
+    /* Stretch the grid in the x axis */
+    for (int u = 1; u <= N; ++u) {
+      for (int v = 1; v <= N; ++v) {
+        previous[linear_from_3d(N + 2, 0, u, v)] = previous[linear_from_3d(N + 2, N, u, v)];
+        previous[linear_from_3d(N + 2, N + 1, u, v)] = previous[linear_from_3d(N + 2, 1, u, v)];
+      }
+    }
+
+    /* Stretch the grid in the y axis */
+    for (int u = 0; u <= N + 1; ++u) {
+      for (int v = 1; v <= N; ++v) {
+        previous[linear_from_3d(N + 2, u, 0, v)] = previous[linear_from_3d(N + 2, u, N, v)];
+        previous[linear_from_3d(N + 2, u, N + 1, v)] = previous[linear_from_3d(N + 2, u, 1, v)];
+      }
+    }
+
+    /* Stretch the grid in the z axis */
+    for (int u = 0; u <= N + 1; ++u) {
+      for (int v = 0; v <= N + 1; ++v) {
+        previous[linear_from_3d(N + 2, u, v, 0)] = previous[linear_from_3d(N + 2, u, v, N)];
+        previous[linear_from_3d(N + 2, u, v, N + 1)] = previous[linear_from_3d(N + 2, u, v, 1)];
+      }
+    }
+
     /* Update the cells */
 #pragma omp parallel for schedule(static) collapse(3) reduction(+ : total_count)
-    for (int x = 0; x < N; x++) {
-      for (int y = 0; y < N; y++) {
-        for (int z = 0; z < N; z++) {
-          unsigned char current = read_grid(previous, N, x, y, z);
+    for (int x = 1; x <= N; ++x) {
+      for (int y = 1; y <= N; ++y) {
+        for (int z = 1; z <= N; ++z) {
+          int c = linear_from_3d(N + 2, x, y, z);
+          unsigned char current = previous[c];
+
+          /**
+           * README:
+           *
+           * The following code is a manual unrolling of the loop that checks the
+           * neighbors of a cell. This is done to ensure we access the grid in the
+           * optimal way, cache-wise. We compared the performance with the original
+           * loop using #pragma GCC unroll, but we got 30% worse performance.
+           *
+           * We also omit the line that would check the current cell, as we only want
+           * to check the neighbors.
+           */
 
           if (current) {
             int live_neighbors = 0;
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 0, 0, -1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 0, 0, 1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, -1, -1, -1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, -1, -1, 0);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, -1, -1, 1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, -1, 0, -1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, -1, 0, 0);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, -1, 0, 1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, -1, 1, -1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, -1, 1, 0);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, -1, 1, 1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 0, -1, -1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 0, -1, 0);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 0, -1, 1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 0, 1, -1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 0, 1, 0);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 0, 1, 1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 1, -1, -1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 1, -1, 0);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 1, -1, 1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 1, 0, -1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 1, 0, 0);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 1, 0, 1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 1, 1, -1);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 1, 1, 0);
-            live_neighbors += !!read_neighbor(previous, N, x, y, z, 1, 1, 1);
+
+            // We first access the neighbors in the same line as the current cell.
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 0, 0, -1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 0, 0, 1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, -1, -1, -1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, -1, -1, 0);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, -1, -1, 1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, -1, 0, -1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, -1, 0, 0);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, -1, 0, 1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, -1, 1, -1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, -1, 1, 0);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, -1, 1, 1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 0, -1, -1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 0, -1, 0);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 0, -1, 1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 0, 1, -1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 0, 1, 0);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 0, 1, 1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 1, -1, -1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 1, -1, 0);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 1, -1, 1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 1, 0, -1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 1, 0, 0);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 1, 0, 1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 1, 1, -1);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 1, 1, 0);
+            live_neighbors += is_neighbor_alive(previous, N + 2, c, 1, 1, 1);
             if (live_neighbors <= 4 || live_neighbors > 13) {
               current = 0;
             }
           } else {
+            // Same as above, but we collect the counts for each species.
             int neighbor_count[N_SPECIES + 1] = {0};
-            neighbor_count[read_neighbor(previous, N, x, y, z, 0, 0, -1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 0, 0, 1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, -1, -1, -1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, -1, -1, 0)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, -1, -1, 1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, -1, 0, -1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, -1, 0, 0)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, -1, 0, 1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, -1, 1, -1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, -1, 1, 0)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, -1, 1, 1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 0, -1, -1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 0, -1, 0)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 0, -1, 1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 0, 1, -1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 0, 1, 0)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 0, 1, 1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 1, -1, -1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 1, -1, 0)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 1, -1, 1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 1, 0, -1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 1, 0, 0)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 1, 0, 1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 1, 1, -1)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 1, 1, 0)] += 1;
-            neighbor_count[read_neighbor(previous, N, x, y, z, 1, 1, 1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 0, 0, -1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 0, 0, 1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, -1, -1, -1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, -1, -1, 0)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, -1, -1, 1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, -1, 0, -1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, -1, 0, 0)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, -1, 0, 1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, -1, 1, -1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, -1, 1, 0)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, -1, 1, 1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 0, -1, -1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 0, -1, 0)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 0, -1, 1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 0, 1, -1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 0, 1, 0)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 0, 1, 1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 1, -1, -1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 1, -1, 0)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 1, -1, 1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 1, 0, -1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 1, 0, 0)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 1, 0, 1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 1, 1, -1)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 1, 1, 0)] += 1;
+            neighbor_count[read_neighbor(previous, N + 2, c, 1, 1, 1)] += 1;
 
+            // To get the number of live neighbors, we subtract the number of dead neighbors
+            // from the total number of neighbors.
             int live_neighbors = 26 - neighbor_count[0];
             if (live_neighbors >= 7 && live_neighbors <= 10) {
-              neighbor_count[1] = (neighbor_count[1] << 4) | (N_SPECIES - 1);
-              neighbor_count[2] = (neighbor_count[2] << 4) | (N_SPECIES - 2);
-              neighbor_count[3] = (neighbor_count[3] << 4) | (N_SPECIES - 3);
-              neighbor_count[4] = (neighbor_count[4] << 4) | (N_SPECIES - 4);
-              neighbor_count[5] = (neighbor_count[5] << 4) | (N_SPECIES - 5);
-              neighbor_count[6] = (neighbor_count[6] << 4) | (N_SPECIES - 6);
-              neighbor_count[7] = (neighbor_count[7] << 4) | (N_SPECIES - 7);
-              neighbor_count[8] = (neighbor_count[8] << 4) | (N_SPECIES - 8);
-              neighbor_count[9] = (neighbor_count[9] << 4) | (N_SPECIES - 9);
+#pragma GCC unroll 9
+              for (int i = 1; i <= N_SPECIES; ++i) {
+                // We store the species count in the lower 4 bits of the neighbor count, in order to
+                // use a branchless max. Notice that we store the species as N_SPECIES - i, so that
+                // the species with a lower number have higher priority in case of a tie.
+                neighbor_count[i] = (neighbor_count[i] << 4) | (N_SPECIES - i);
+              }
 
+              // Use a branchless max to find the maximum of the values computed above.
               int maximum = neighbor_count[1];
-              maximum = fast_max(maximum, neighbor_count[2]);
-              maximum = fast_max(maximum, neighbor_count[3]);
-              maximum = fast_max(maximum, neighbor_count[4]);
-              maximum = fast_max(maximum, neighbor_count[5]);
-              maximum = fast_max(maximum, neighbor_count[6]);
-              maximum = fast_max(maximum, neighbor_count[7]);
-              maximum = fast_max(maximum, neighbor_count[8]);
-              maximum = fast_max(maximum, neighbor_count[9]);
+#pragma GCC unroll 9
+              for (int i = 2; i <= N_SPECIES; ++i) {
+                maximum = fast_max(maximum, neighbor_count[i]);
+              }
+
+              // Extract the species from the lower 4 bits of the maximum value.
               current = N_SPECIES - (maximum & 0x0F);
             }
           }
 
           total_count[current] += 1;
-          write_grid(next, N, x, y, z, current);
+          next[c] = current;
         }
       }
     }
@@ -252,7 +288,7 @@ int main(int argc, char **argv) {
 
   /* Stop tracking time */
   time += omp_get_wtime();
-  printf("%.1fs\n", time);
+  fprintf(stderr, "%.1fs\n", time);
 
   /* Print the maximums */
   for (int i = 1; i <= N_SPECIES; i++) {
